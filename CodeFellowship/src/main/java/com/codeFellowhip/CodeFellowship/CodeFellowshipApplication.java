ef70e1b20b4b25a0e45ead3ea273d7a2374ebd72
Nicholas Brown <119603466+NicholasBrown-01@users.noreply.github.com>
2023-07-31 12:56:32 -0500

    Add files via upload
    Initial Doc Setup

---
+++ CodeFellowship/src/main/java/com/codeFellowhip/CodeFellowship/CodeFellowshipApplication.java
@@ -0,0 +1,13 @@
+package com.codeFellowhip.CodeFellowship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeFellowshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeFellowshipApplication.class, args);
+	}
+
+}
---
+++ CodeFellowship/src/test/java/com/codeFellowhip/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeFellowhip.CodeFellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
